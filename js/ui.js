011146105cf1d3954c00a023b75b8fe83251a057
choiwjun <31870430+choiwjun@users.noreply.github.com>
2021-07-07 16:01:13 +0900

    test

--- js/ui.js
+++ js/ui.js
@@ -3,7 +3,7 @@ var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform ) { 
     if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
         //mobile 
-        location.href = "./mobile/index.html";
+        location.replace = "mobile/index.html";
     } else { 
         //pc 
         
